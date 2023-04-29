e817812cccdd215e567045f605190b26f698fa90
chaerlo127 <monynony0203@gmail.com>
2023-04-30 03:40:51 +0900

    midterm: error 해결

--- midterm/Compiler_MIDTERM/src/parser/SCodeSegment.java
+++ midterm/Compiler_MIDTERM/src/parser/SCodeSegment.java
@@ -24,7 +24,7 @@ public class SCodeSegment implements INode {
 
 				SSymbolEntity entity = new SSymbolEntity();
 				entity.setVariableName(operator.replace(":", ""));
-				entity.setValue(this.statements.size()-1); // pc count
+				entity.setValue(this.statements.size()); // pc count
 				this.symbolTable.add(entity);
 			} else {
 				// parse tree

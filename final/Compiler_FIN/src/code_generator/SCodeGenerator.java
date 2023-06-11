85cbd021b39d96c8bd0bc623a9e154355b1e812d
chaerlo127 <monynony0203@gmail.com>
2023-06-12 03:40:03 +0900

    0612: code generator 생성

---
+++ final/Compiler_FIN/src/code_generator/SCodeGenerator.java
@@ -0,0 +1,85 @@
+package code_generator;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Vector;
+
+import constant.Constant;
+import instruction_design.SOperator;
+import parser.SStatement;
+import parser.SSymbolEntity;
+import parser.SSymbolTable;
+
+public class SCodeGenerator {
+	private SSymbolTable symbolTable;
+	private Vector<SStatement> statements;
+	private Vector<String> generationCode;
+	
+
+	public void connect(SSymbolTable symbolTable, Vector<SStatement> statements, Vector<String> generationCode) {
+		this.symbolTable = symbolTable;
+		this.statements = statements;
+		this.generationCode = generationCode;
+	}
+	
+	public void generate() {
+		for (SStatement statement : statements) {
+			String code = null;
+			System.out.println(statement.getOperator());
+			// operator
+			SOperator operator = SOperator.findByAssemblyCode(statement.getOperator());
+			code = Constant.CCodeGenerator.hexaCodeGenerate(operator.getCode());
+
+			// operand1
+			if (statement.getOperand() != null) {
+				code += translateOperand(statement.getOperand());
+			}
+			generationCode.add(code);
+		}
+	}
+	
+
+	public void createExeFile(String fileName) {
+		try {
+			File exeFile = new File(Constant.CCodeGenerator.EXEDirectory + fileName);
+			FileWriter fw = new FileWriter(exeFile);
+			
+			for(String code: generationCode) {
+				fw.write(code + "\n");
+			}
+			fw.flush();
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	
+	/**
+	 * extract methods
+	 * @param String isAt
+	 * @return String isAt
+	 */
+	private String deleteAt(String isAt) {
+		if(isAt.contains("@")) {
+			isAt = isAt.replace("@", "");
+		}
+		return isAt;
+	}
+	
+	private String translateOperand(String operand) {
+		// operand2
+		operand = this.deleteAt(operand);
+		SSymbolEntity entity = this.symbolTable.findByVariableName(operand);
+		if (entity != null) {
+			return Constant.CCodeGenerator.PRINT_HEXA_SIGN + Integer.toHexString(entity.getValue()) + " ";
+		} else {
+				return Constant.CCodeGenerator.hexaCodeGenerate(Integer.parseInt(operand));
+
+		}
+		
+	}
+
+	
+	
+}
--- final/Compiler_FIN/src/main/SMain.java
+++ final/Compiler_FIN/src/main/SMain.java
@@ -3,6 +3,7 @@ package main;
 import java.io.File;
 import java.util.Vector;
 
+import code_generator.SCodeGenerator;
 import lex.SLex;
 import parser.SParser;
 import parser.SStatement;
@@ -13,7 +14,8 @@ public class SMain {
 
 	private SSymbolTable symbolTable;
 	private Vector<SStatement> statements;
-//	private Vector<String> generationCode;
+	private SCodeGenerator codeGenerator;
+	private Vector<String> generationCode;
 	
 	private SLex lex;
 	private SParser parser;
@@ -29,16 +31,19 @@ public class SMain {
 	private void run() {
 		this.lex.initialize(new File("assembly/exe"));
 		parser.parse(this.lex, symbolTable, statements); // 어떤 파일을 파싱해라
+		codeGenerator.connect(symbolTable, statements, generationCode);
+		codeGenerator.generate();
+		codeGenerator.createExeFile("exe");
 	}
 
 	private void initialize() {
 		// 공용 데이터
 		symbolTable = new SSymbolTable();
 		statements = new Vector<SStatement>();
-//		generationCode = new Vector<>();
+		generationCode = new Vector<>();
 		lex = new SLex();
 		parser = new SParser();
-//		codeGenerator = new SCodeGenerator();  
+		codeGenerator = new SCodeGenerator();  
 
 //		this.files = new Vector<>();
 	}

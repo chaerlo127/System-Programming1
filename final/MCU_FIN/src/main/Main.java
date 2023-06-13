b0b57a9faddd71450470b745d55e452c9ea9882f
chaerlo127 <monynony0203@gmail.com>
2023-06-13 21:38:31 +0900

    0613: exe1 파일 생성

--- final/MCU_FIN/src/main/Main.java
+++ final/MCU_FIN/src/main/Main.java
@@ -23,16 +23,24 @@ public class Main {
 	}
 
 	public void run() {
-		System.out.println("[MCU] (1) exe (2) 끝내기");
+		System.out.println("[MCU] (1) exe (2) exe1 (0) 끝내기");
 		int input = scanner.nextInt();
 		while (input != 0){
-			memory.setFile(new File("exe/exe"));
-			memory.parse();
-			cpu.start("exe");
-			System.out.println("[MCU] (1) exe (2) 끝내기");
+			if(input == 1) input = selectFile("exe/exe");
+			if(input == 2) input = selectFile("exe/exe1");
+			else System.out.println("다시 입력해주세요!");
 			input = scanner.nextInt();
 		}
 		scanner.close();
+	}
 
+	private int selectFile(String fileName) {
+		int input;
+		memory.setFile(new File(fileName));
+		memory.parse();
+		cpu.start(fileName);
+		System.out.println("[MCU] (1) exe (2) exe1 (0) 끝내기");
+		input = scanner.nextInt();
+		return input;
 	}
 }
